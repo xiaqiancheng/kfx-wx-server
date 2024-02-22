08fe126e50116ca66827a66af6055c9762b2f4f1
xiaqiancheng <xiaqiancheng@shopex.cn>
2024-02-22 22:25:54 +0800

    消息通知表

---
+++ migrations/2024_02_22_141547_create_message_notice_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Hyperf\Database\Schema\Schema;
+use Hyperf\Database\Schema\Blueprint;
+use Hyperf\Database\Migrations\Migration;
+
+class CreateMessageNoticeTable extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('message_notice', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->bigInteger('task_id')->default(0)->comment('任务ID');
+            $table->bigInteger('blogger_id')->default(0)->comment('博主ID');
+            $table->string('name')->default('')->comment('名称');
+            $table->string('description')->default('')->comment('描述');
+            $table->tinyInteger('status')->default(0)->comment('读取状态 0未读 1已读');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('message_notice');
+    }
+}
