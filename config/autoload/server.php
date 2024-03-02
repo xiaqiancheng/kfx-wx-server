6dd24dff99a5ee21cfa7046e8f2a41f7b62f53d4
xiaqiancheng <xiaqiancheng@shopex.cn>
2024-03-02 14:36:35 +0800

    修改端口

--- config/autoload/server.php
+++ config/autoload/server.php
@@ -20,7 +20,7 @@ return [
             'name' => 'http',
             'type' => Server::SERVER_HTTP,
             'host' => '0.0.0.0',
-            'port' => 9552,
+            'port' => 9551,
             'sock_type' => SWOOLE_SOCK_TCP,
             'callbacks' => [
                 Event::ON_REQUEST => [Hyperf\HttpServer\Server::class, 'onRequest'],
